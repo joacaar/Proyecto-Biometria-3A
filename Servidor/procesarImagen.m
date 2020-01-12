b0c2e3498252cc45cec3c37bd4c506e3b644bfa4
Adrian <36443291+AdrHer13@users.noreply.github.com>
2020-01-12 20:08:39 +0100

    Creada script de matlab
    Binariza una imagen de manera automática. La imagen ya puesta es para que se conserven las carpetas en github y de paso se vea como ejemplo

---
+++ Servidor/procesarImagen.m
@@ -0,0 +1,16 @@
+function procesarImagen()
+image=imread('./Imagenes_A_Procesar/paraProcesar.png');
+
+%  Se comprueba si es escala de grises y si no lo es se vuelve escala de
+%  grises
+%------------------------------------------------------------
+if size(image,3)==3
+    image=rgb2gray(image);
+end
+
+T = graythresh(image);
+BW = imbinarize(image, T);
+BW2 = imcomplement(BW);
+
+imwrite(BW2, './Imagenes_Procesadas/binaria.bmp')
+end
